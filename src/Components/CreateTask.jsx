06a8043b89c1340896aa96edfb643f7814855d8e
programming hero <mrrohan1574@gmail.com>
2024-01-24 11:18:43 +0600

    first commit

---
+++ src/Components/CreateTask.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Modal from "../Components/Modal"
+import { v4 as uuidv4 } from 'uuid';
+import toast from 'react-hot-toast';
+
+const CreateTask = ({tasks, setTasks}) => {
+    const [open, setOpen] = useState(false);
+    const [task, setTask] = useState({
+        id: "",
+        name: "",
+        status: "todo",
+    })
+    console.log(task)
+    const handle = (e) => {
+        e.preventDefault();
+        if(task.name.length < 3) return toast.error("A task must have more than 3 characters")
+            if(task.name.length > 40) return toast.error("A task must not be more  than 40 characters")
+        setTasks((prev) =>{
+            const lists = [...prev, task]
+            
+            localStorage.setItem("task", JSON.stringify(lists))
+            return lists;
+        })
+        
+        setTask({
+            id: "",
+            name: "",
+            status: "todo",
+        });
+        toast.success('Successfully toasted!')
+    };
+    return (
+        <div className='flex justify-end mt-5 bg-green-400 h-56'>
+            <div>
+            <button className='border border-neutral-300 rounded-lg py-1.5 px-8 mr-10 mt-10 bg-white text-2xl font-bold  text-green-400' onClick={() => setOpen(true)}>
+                New Task
+            </button>
+            </div>
+
+            <Modal open={open} onClose={() => setOpen(false)}>
+                <div className="text-center w-96 h-40">
+
+                    <div size={56} className="mx-auto text-green-500" />
+                    <div className="mx-auto ">
+                        <form onSubmit={handle}>
+                        {/* <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" /> */}
+                            <input placeholder="Type here" className='border mr-4  border-slate-400 bg-slate-100 rounded-md h-20 w-60' required type="text" value={task.name} onChange={(e)=> setTask({...task, id: uuidv4(), name:e.target.value })} />
+                            
+                            <button className='bg-cyan-500 rounded-md px-4 h-12 text-white'>create</button>
+                        </form>
+                    </div>
+                    <div className="flex justify-end mt-6 gap-4">
+                        
+                        <button onClick={() => setOpen(false)} className=" bg-cyan-500 rounded-md px-4 h-12 text-white">Cancel</button>
+
+                    </div>
+                </div>
+            </Modal>
+        </div>
+    );
+};
+
+export default CreateTask;
---
+++ src/Components/ListTask.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import toast from 'react-hot-toast';
+
+
+const ListTask = ({ tasks, setTasks }) => {
+    const [todos, setTodos] = useState([]);
+    const [inProgress, setInProgress] = useState([]);
+    // const [done, setDone] = useState([]);
+    const [closed, setClosed] = useState([]);
+
+    useEffect(() => {
+        const fTodos = tasks.filter((task) => task.status === "todo");
+        const fInprogress = tasks.filter((task) => task.status === "inprogress");
+        const fClosed = tasks.filter((task) => task.status === "closed");
+
+        setTodos(fTodos);
+        setInProgress(fInprogress);
+        setClosed(fClosed);
+    }, [tasks]);
+    const statues = ["todo", "inprogress", "closed"]
+    return (
+        <div className='flex gap-16'>
+            {statues.map((status, index) => (
+                <Section key={index} status={status} tasks={tasks} setTasks={setTasks} todos={todos} inProgress={inProgress} closed={closed} ></Section>
+            ))}
+        </div>
+    );
+};
+
+export default ListTask;
+const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: "task",
+        drop: (item) => addItemToSection(item.id),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver()
+        })
+    }))
+
+    let text = "Todo";
+    let bgs = "text-green-400  border  border-green-500 hover:border-blue-500 hover:text-blue-600";
+    let texts = "text-green-400 hover:text-red-500";
+    let bg= "text-black text-xl ";
+    let tasksToMap = todos;
+
+    if (status === "inprogress") {
+        text = "In Progress";
+        texts = "text-green-400 hover:text-red-500";
+        bgs = 'text-slate-400  border border-blue-500 hover:border-green-500';
+        
+       bg='text-black text-xl';
+        tasksToMap = inProgress;
+    }
+    if (status === "closed") {
+        text = "done";
+        bgs = 'text-slate-400 ';
+        bg='bg-slate-300 text-black text-xl';
+        texts = "text-green-400 hover:text-red-500" ;
+        tasksToMap = closed;
+    };
+    const addItemToSection = (id) => {
+        setTasks((prev) => {
+            const mTasks = prev.map((task) => {
+                if (task.id === id) {
+                    return { ...task, status: status }
+                }
+                return task;
+            })
+            localStorage.setItem("task", JSON.stringify(mTasks));
+            toast('Task Status Changed', {
+                icon: '👏',
+            });
+            return mTasks;
+        })
+    }
+    return (
+        <div ref={drop}  className={`w-80 rounded-md p-4 ml-12 bg-slate-100  -mt-10 ${isOver ? "bg-slate-200" : "" }`}  >
+            <Header text={text} bgs={texts}  count={tasksToMap.length}></Header>
+            {
+                tasksToMap.length > 0 && tasksToMap.map((task) => (
+                    <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} bgs={bgs} bg={bg} ></Task>
+
+                ))
+            }
+        </div>
+    );
+};
+const Header = ({ text, count, bgs, bg ,texts}) => {
+    return (
+        <div className=''>
+            <div className={`${bgs} ${bg} ${texts} flex  items-center justify-center h-12 pl-4 rounded-md uppercase text-2xl font-bold text-green-500`}>
+                {text} 
+                <div className={`ml-2  ${bgs} bg-white w-5 h-5  rounded-full flex items-center justify-center`}>
+                    {count}
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+const Task = ({ task, tasks, setTasks, bgs,bg }) => {
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "task",
+        item: { id: task.id },
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
+    console.log(isDragging)
+    const handleRemove = (id) => {
+
+        const fTasks = tasks.filter((t) => t.id !== id);
+        localStorage.setItem("task", JSON.stringify(fTasks))
+        setTasks(fTasks);
+        toast("remove")
+
+    }
+    return <div >
+        <div ref={drag} className={`${bgs} ${bg}  relative p-4 mt-8  border shadow-md bg-white rounded-md cursor-grab ${isDragging ? "opacity-25 " : "opacity-100 "}`}>
+            <p className='text-center' >  {task.name}</p> 
+            <button className='absolute bottom-1 right-1  text-slate-400' onClick={() => handleRemove(task.id)}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+
+            </button>
+        </div>
+    </div>
+}
